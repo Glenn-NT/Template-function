7df079d3c4040a091e31ed4d178a6b85bb20afe8
glennM <glennmoonie001@gmail.com>
2021-05-01 13:51:32 -0400

    Defined function template half that returns half of the value passed to it.

--- Template function/Source.cpp
+++ Template function/Source.cpp
@@ -6,11 +6,19 @@
 
 using namespace std;
 
+template<class T>
+T half(T num) {
 
+	return num / 2;
+}
 
 int main() {
 
+	double a = 5;
+	float b = 7.0f;
 
+	cout << half(a) << endl;
+	cout << half(b) << endl;
 
 	system("pause");
 	return 0;
